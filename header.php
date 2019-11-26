04cf8dc3246a119fe7998cc0c6b8198ffb1c6641
David Peach <mail@davidpeach.co.uk>
2019-11-26 11:19:58 +0000

    Testing deploy workflow

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<!-- test deploy workflow -->
 	<?php wp_head(); ?>
 </head>
 
